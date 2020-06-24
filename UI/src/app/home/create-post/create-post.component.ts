22034ba4bec7db942511eb4cfd8cc136218f2675
Maverick <feruchio599@gmail.com>
2020-06-24 20:07:13 +0300

    home layout finished and create post work

--- UI/src/app/home/create-post/create-post.component.ts
+++ UI/src/app/home/create-post/create-post.component.ts
@@ -5,30 +5,48 @@ import { AuthState } from '../../store/auth/auth.reducer';
 import { User } from '../../users/models/dto/user';
 import { takeUntil, filter } from 'rxjs/operators';
 import { authState } from '../../store/auth/auth.selectors';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.scss']
+  styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent extends BaseComponent implements OnInit {
   isLoggedIn: boolean;
   user: User;
+  postForm: FormGroup;
 
   constructor(private store: Store<AuthState>) {
     super();
 
-    store.pipe(
-      takeUntil(this.destroyed$),
-      select(authState),
-      filter(x => !!x)
-    ).subscribe(x => {
-      this.isLoggedIn = x.isAuthenticated;
-      this.user = x.profile;
-    });
-   }
-
-  ngOnInit(): void {
+    store
+      .pipe(
+        takeUntil(this.destroyed$),
+        select(authState),
+        filter(x => !!x),
+      )
+      .subscribe(x => {
+        this.isLoggedIn = x.isAuthenticated;
+        this.user = x.profile;
+      });
   }
 
+  ngOnInit(): void {
+    this.postForm = new FormGroup({
+      content: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(5000),
+      ]),
+    });
+  }
+
+  get content() {
+    return this.postForm.get('content');
+  }
+
+  onPost() {
+
+  }
 }
--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
   opened = true;
+  mode = 'side';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(['(max-width: 768px)']).subscribe(result => {
+      if (result.matches) {
+        this.opened = false;
+        this.mode = 'over';
+      } else {
+        this.opened = true;
+        this.mode = 'side';
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  onToggle() {
+    this.sidenav.toggle();
+    this.opened = !this.opened;
+  }
 }
