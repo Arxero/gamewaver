20fa86b7653f215164786eb1dc120f1c847d697b
Maverick <feruchio599@gmail.com>
2020-08-07 23:23:51 +0300

    cors url change

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('host.port');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: ['http://localhost:4200', /\.gamewaver\.com$/] });
+  app.enableCors({ origin: ['http://gamewaver.com', 'http://localhost:4200'] });
 
   const options = new DocumentBuilder()
     .setTitle('Gamewaver API')
