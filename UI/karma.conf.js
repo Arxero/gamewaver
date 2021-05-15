96af51278b029547978e2cc91cf449b6753a0507
Maverick <feruchio599@gmail.com>
2021-05-15 18:15:21 +0300

    renaming the project from gw-angular to gamewaver

--- UI/karma.conf.js
+++ UI/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/gw-angular'),
+      dir: require('path').join(__dirname, './coverage/gamewaver'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
